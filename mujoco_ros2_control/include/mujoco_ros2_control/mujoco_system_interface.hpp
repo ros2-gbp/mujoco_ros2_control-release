05d4d7da81c1903b29888d38e25f2178ea2429a7
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2026-03-17 09:42:00 +0100

    Rebase from 'upstream'

--- mujoco_ros2_control/include/mujoco_ros2_control/mujoco_system_interface.hpp
+++ mujoco_ros2_control/include/mujoco_ros2_control/mujoco_system_interface.hpp
@@ -52,6 +52,7 @@
 
 #include <pluginlib/class_list_macros.hpp>
 #include <pluginlib/class_loader.hpp>
+#include "mujoco_ros2_control_plugins/mujoco_ros2_control_plugins_base.hpp"
 #include "transmission_interface/transmission.hpp"
 #include "transmission_interface/transmission_interface_exception.hpp"
 #include "transmission_interface/transmission_loader.hpp"
@@ -296,6 +297,15 @@ private:
    */
   rclcpp::Node::SharedPtr get_node() const;
 
+  /// Load plugins based on ROS parameters.
+  /**
+   * This method looks for parameters under the "mujoco_plugins" namespace, and attempts to load plugins based on the
+   * unique keys found there. For example, if a parameter "mujoco_plugins.my_plugin.some_param" is found, this method
+   * will attempt to load a plugin with the key "my_plugin". The plugins should be specified in the ROS parameter server
+   * before the system interface is initialized, and should follow the naming convention described above.
+   */
+  void load_mujoco_plugins();
+
   // System information
   std::string model_path_;
 
@@ -376,6 +386,11 @@ private:
   // Transmission instances
   std::unique_ptr<pluginlib::ClassLoader<transmission_interface::TransmissionLoader>> transmission_loader_ = nullptr;
   std::vector<std::shared_ptr<transmission_interface::Transmission>> transmission_instances_;
+
+  // Plugin loader and instances
+  std::unique_ptr<pluginlib::ClassLoader<mujoco_ros2_control_plugins::MuJoCoROS2ControlPluginBase>> plugin_loader_ =
+      nullptr;
+  std::vector<std::shared_ptr<mujoco_ros2_control_plugins::MuJoCoROS2ControlPluginBase>> plugin_instances_;
 
   std::vector<FTSensorData> ft_sensor_data_;
   std::vector<IMUSensorData> imu_sensor_data_;
