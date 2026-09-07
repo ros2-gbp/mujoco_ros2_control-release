6a765976dfd88f9673c78c523f9f8882aae7e505
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2026-09-07 17:43:46 +0200

    Rebase from 'upstream'

--- mujoco_ros2_control_tests/src/test_plugin.cpp
+++ mujoco_ros2_control_tests/src/test_plugin.cpp
@@ -1,4 +1,5 @@
 #include <pluginlib/class_list_macros.hpp>
+#include <std_msgs/msg/u_int32.hpp>
 #include "mujoco_ros2_control_plugins/mujoco_ros2_control_plugins_base.hpp"
 
 namespace external_plugin_test
@@ -7,16 +8,30 @@ namespace external_plugin_test
 class TestPlugin : public mujoco_ros2_control_plugins::MuJoCoROS2ControlPluginBase
 {
 public:
-  bool init(rclcpp::Node::SharedPtr, const mjModel*, mjData*) override
+  bool init(rclcpp::Node::SharedPtr node, const mjModel*, mjData*) override
   {
+    // Latched so a test subscribing after a reset still observes the count.
+    world_reset_count_pub_ =
+        node->create_publisher<std_msgs::msg::UInt32>("world_reset_count", rclcpp::QoS(1).transient_local());
     return true;
   }
   void update(const mjModel*, mjData*) override
   {
   }
+  void on_reset(mjData*) override
+  {
+    std_msgs::msg::UInt32 msg;
+    msg.data = ++world_reset_count_;
+    world_reset_count_pub_->publish(msg);
+  }
   void cleanup() override
   {
+    world_reset_count_pub_.reset();
   }
+
+private:
+  rclcpp::Publisher<std_msgs::msg::UInt32>::SharedPtr world_reset_count_pub_;
+  uint32_t world_reset_count_{ 0 };
 };
 
 }  // namespace external_plugin_test
